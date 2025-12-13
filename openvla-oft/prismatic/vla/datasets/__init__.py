817abd2202aa8fc72bb791597ce9c87e7062b1c0
jiiiisoo <jisoo6687@yonsei.ac.kr>
2025-12-14 02:08:08 +0900

    add tfds

--- openvla-oft/prismatic/vla/datasets/__init__.py
+++ openvla-oft/prismatic/vla/datasets/__init__.py
@@ -1,1 +1,2 @@
 from .datasets import DummyDataset, EpisodicRLDSDataset, RLDSBatchTransform, RLDSDataset
+from .libero_hdf5_dataset import LIBEROHdf5Dataset, LIBEROBatchTransform, make_libero_hdf5_datasets
